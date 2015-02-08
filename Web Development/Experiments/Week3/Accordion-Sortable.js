6fc7e1662d5c62d273a34f04c4d480b29efc56e5
Vikas Shriyan <shriyan.v@husky.neu.edu>
2015-02-07 20:31:00 -0500

    Week3
    Draggable and Accordion-Sortable Experiments

---
+++ Web Development/Experiments/Week3/Accordion-Sortable.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    $("#accordion")
+      .accordion({
+          header: "> div > h3"
+      })
+      .sortable({
+          axis: "y",
+          handle: "h3",
+          stop: function (event, ui) {
+              $(this).accordion("refresh");
+          }
+      });
+});
---
+++ Web Development/Experiments/Week3/Draggable.js
@@ -0,0 +1,35 @@
+﻿$(init);
+function init() {
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+
+    $('#drag-me').draggable(); // drag all over the page
+    $('#drag-me-hor').draggable({ axis: "x" }); // drag horizontally on x-axis
+    $('#drag-me-ver').draggable({ axis: "y" }); // drag vertically on y-axis
+    $('#drag-me-grid').draggable({ grid: [25, 25] });
+}
+
+function allOver(obj) {
+    var p = $(obj);
+    var position = p.position();
+    var pos = "Left Spacing : " + position.left + "px" + "<br/>Top Spacing : " + position.top + "px";
+    $("#drag-me-allOver").empty().append(pos);
+}
+function Hor(obj) {
+    var p = $(obj);
+    var position = p.position();
+    var pos = "Left Spacing : " + position.left + "px" + "<br/>Top Spacing : " + position.top + "px";
+    $("#drag-me-Hor").empty().append(pos);
+}
+function Ver(obj) {
+    var p = $(obj);
+    var position = p.position();
+    var pos = "Left Spacing : " + position.left + "px" + "<br/>Top Spacing : " + position.top + "px";
+    $("#drag-me-Ver").empty().append(pos);
+}
+function Grid(obj) {
+    var p = $(obj);
+    var position = p.position();
+    var pos = "Left Spacing : " + position.left + "px" + "<br/>Top Spacing : " + position.top + "px";
+    $("#drag-me-Grid").empty().append(pos);
+}
