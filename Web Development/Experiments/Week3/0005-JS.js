8450e3564683a1a4f82e488ede9d447dc2938a11
Vikas Shriyan <shriyan.v@husky.neu.edu>
2015-02-09 15:14:25 -0500

    Week3
    Blog entry and last experiment

---
+++ Web Development/Experiments/Week3/0005-JS.js
@@ -0,0 +1,41 @@
+﻿$(document).ready(function () {
+
+    $(".CSS1").click(function () {
+        $(this).css({
+            "background-color": "blue",
+            "color":"white",
+            "font-style": "italic",
+            "fontFamily": "Impact,Charcoal,sans-serif"
+        })
+    });
+
+    $(".CSS2").click(function () {
+        $(this).css({
+            "background-color": "red",
+            "font-style": "bold",
+            "fontFamily": "Courier New"
+        })
+    });
+
+    $(".CSS3").click(function () {
+        $(".CSS3").css({
+            "background-image": 'url("http://upload.wikimedia.org/wikipedia/en/c/cc/Chelsea_FC.svg")',
+            "width": "210px",
+            "height": "210px",
+            "font-size": "0"
+        })
+    });
+
+    $(".CSS4").click(function () {
+        $(".CSS4").css({
+            "background-image": 'url("http://upload.wikimedia.org/wikipedia/en/5/53/Arsenal_FC.svg")',
+            "width": "322px",
+            "height": "379px",
+            "font-size": "0"
+        })
+    });
+
+    $('#refresh').click(function () {
+        location.reload();
+    });
+});
