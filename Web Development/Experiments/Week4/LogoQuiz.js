c459cda434e2933698be13d92c124a1f752ab1ac
Vikas Shriyan <shriyan.v@husky.neu.edu>
2015-02-14 15:07:26 -0500

    Week4
    Exp6

---
+++ Web Development/Experiments/Week4/LogoQuiz.js
@@ -0,0 +1,38 @@
+﻿var app=angular.module("logoQuiz",[])
+
+app.controller("LogoController", function ($scope) {
+    $scope.answer = "";
+
+    $scope.realmadrid = function () {
+        $scope.answer = "Real Madrid";
+
+    };
+    $scope.manunited = function () {
+        $scope.answer = "Manchester United";
+
+    };
+    $scope.arsenal = function () {
+        $scope.answer = "Arsenal";
+
+    };
+    $scope.asroma = function () {
+        $scope.answer = "AS Roma";
+
+    };
+    $scope.atm = function () {
+        $scope.answer = "Athletico Madrid";
+
+    };
+    $scope.barcelona = function () {
+        $scope.answer = "Barcelona";
+
+    };
+    $scope.liverpool = function () {
+        $scope.answer = "Liverpool";
+
+    };
+    $scope.mancity = function () {
+        $scope.answer = "Manchester City";
+
+    };
+});
