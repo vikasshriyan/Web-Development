5a245fcd1f2c14d15db8869e38639a63a3029bfa
Vikas Shriyan <shriyan.v@husky.neu.edu>
2015-02-20 18:09:23 -0500

    Week5
    Exp4

---
+++ Web Development/Experiments/Week5/MovieLibrary-Routing.js
@@ -0,0 +1,37 @@
+﻿var app = angular.module("movieApp", ['ngRoute']);
+
+app.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/home', {
+            templateUrl: 'routing/home.html',
+            controller: 'HomeCtrl'
+        }).
+        when('/profile', {
+            templateUrl: 'routing/profile.html',
+            controller: 'ProfileCtrl'
+        }).
+        otherwise({
+            redirectTo: '/home'
+        });
+  }]);
+
+app.controller("HomeCtrl", function ($scope, MovieService) {
+    $scope.searchMovies = function () {
+       MovieService.searchMovies($scope.searchByTitle, function(response){
+           $scope.movies = response;
+    });
+    }
+
+    $scope.addToFavorites = function(movie)
+    {
+        MovieService.addToFavorites(movie);
+    }
+});
+
+
+app.controller("ProfileCtrl", function ($scope, MovieService) {
+    var favorites = MovieService.getFavorites();
+    console.log(favorites);
+    $scope.favoriteMovies = favorites;
+});
