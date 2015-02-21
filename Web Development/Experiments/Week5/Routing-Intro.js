e5081a3e4f70e629e2bd4fabdbb2900da753eec0
Vikas Shriyan <shriyan.v@husky.neu.edu>
2015-02-21 12:40:47 -0500

    Week5
    Exp6

---
+++ Web Development/Experiments/Week5/Routing-Intro.js
@@ -0,0 +1,25 @@
+﻿var app = angular.module("myApp", ['ngRoute']);
+
+app.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/home', {
+            templateUrl: 'ng-Route/home.html',
+
+        }).
+        when('/about', {
+            templateUrl: 'ng-Route/about.html',
+
+        })
+        .when('/contact', {
+            templateUrl: 'ng-Route/contact.html',
+
+        })
+        .when('/profile', {
+            templateUrl: 'ng-Route/profile.html',
+
+        }).
+        otherwise({
+            redirectTo: '/home'
+        });
+  }]);
